13a91dd1077679360984b2563946575ab1d11ca9
Tokosama <anouartoko@gmail.com>
2025-02-16 17:32:20 +0100

    mpdif de la key de cryptage, jwt, Mise en place du Login

--- models/User.js
+++ models/User.js
@@ -31,8 +31,13 @@ UserSchema.pre("save", async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-    expiresIn: "30d",
+  return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
   });
 };
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword,this.password)
+  return isMatch
+}
 module.exports = mongoose.model("User", UserSchema); 
